206b4624cc676ee34347a344f72e600cb351feee
PatricJ13 <62124100+PatricJ13@users.noreply.github.com>
2021-03-06 12:03:33 -0500

    Update script.js
    updated

--- script.js
+++ script.js
@@ -1,33 +1,28 @@
 function compute()
 {
-		//Create variables from input
-    let principal = document.getElementById("principal").value;
-    let rate = document.getElementById("rate").value;
-    let years = document.getElementById("years").value;
-
-    //do math
-    let interest = principal * years * rate / 100;
-    let endYear = new Date().getFullYear() + parseInt(years);
-
-    //compound interest formula
-    let compounded = principal * ((1 + ((rate/100)/12)) ** (12*years))
-
-    //validate the principal. If negative or 0 alert user and focus on principal
+//Variable creation
+    var principal = document.getElementById("principal").value;
+    var rate = document.getElementById("rate").value;
+    var years = document.getElementById("years").value;
+//math for firmula
+    var interest = principal * years * rate / 100;
+    var endYear = new Date().getFullYear() + parseInt(years);
+//simple formula
+    var compounded = principal * ((1 + ((rate/100)/12)) ** (12*years))
+//Alert!!! number less than 1
     if(principal <= 0){
         alert("Enter a positive number");
         document.getElementById("principal").focus();
         return false;
     }
-
-    //display result
+//Answer displayed on page
     document.getElementById("result").innerHTML = `If you deposit <mark>${principal}</mark>,<br>at an interest rate of
     <mark>${rate}</mark>.<br>You will receive an amount of <mark>${interest.toFixed(2)}</mark>,<br>in the year <mark>${endYear}</mark><br>
-    <br>With monthly compounded interest your balance would instead be <mark>${compounded.toFixed(2)}</mark>`;
+    <br>
 }
-
-//display rate % by slider
+//slider function
 function rangeReader()
 {
-    let sliderValue = document.getElementById("rate").value;
+    var sliderValue = document.getElementById("rate").value;
     document.getElementById("sliderValue").innerText = `${sliderValue}%`;
 }
