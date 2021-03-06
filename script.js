4d1c74e4e1ab08c6d28f604372f2584e56358914
PatricJ13 <62124100+PatricJ13@users.noreply.github.com>
2021-03-06 13:17:05 -0500

    Update script.js
    update

--- script.js
+++ script.js
@@ -7,15 +7,15 @@ function compute()
 //math for firmula
     var interest = principal * years * rate / 100;
     var forYear = new Date().getFullYear() + parseInt(years);
-//Alert!!! number less than 1
+//Alert!!! number less than 1 and focus
     if(principal <= 0){
         alert("Enter a positive number");
         document.getElementById("principal").focus();
         return false;
     }
 //Answer displayed on page
-    document.getElementById("result").innerHTML = 'If you deposit <mark>${principal}</mark>,<br>at an interest rate of
-    <mark>${rate}</mark>.<br>You will receive an amount of <mark>${interest.toFixed(2)}</mark>,<br>in the year <mark>${forYear}</mark>';
+   document.getElementById("result").innerHTML = `If you deposit <mark>${principal}</mark>,<br>at an interest rate of 
+    <mark>${rate}</mark>.<br>You will receive an amount of <mark>${interest.toFixed(2)}</mark>,<br>in the year <mark>${endYear}</mark><br>`;
 }
 //slider value
 function rangeReader()
